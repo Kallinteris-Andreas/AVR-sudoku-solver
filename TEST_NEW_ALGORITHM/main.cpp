bafc8beec5422e85a7c555629e38ffb43d6e536c
Lioudakis Emmanouil <lioudman@gmail.com>
2021-12-03 14:30:31 +0200

    Added comments to main.cpp and Sudoku.h
    Today, after 20:00 we 'll have the first test on STK500. More info tonight.

--- TEST_NEW_ALGORITHM/main.cpp
+++ TEST_NEW_ALGORITHM/main.cpp
@@ -1,5 +1,5 @@
 /*
-; main.c
+; main.cpp
 ;
 ; Course: HRY411 - Embedded Microprocessor Systems, Winter Semester of acad. year 2021-22
 ;
@@ -12,46 +12,55 @@
 ; CPU freq             : 10 MHz, using an oscillator in STK500 (If changed, things such as timer init. value and UBRR for the serial port should change too)
 */
 
-#include "Sudoku.h"
+#include "sudoku.h"
 #include "kallinteris.hpp"
 #include "LUT.hpp"
 #include "sudoku.cpp"
 
-/* An instance of the sudoku class */
+/* An instance of the sudoku class (keeps the grid and the number of completed cells) */
 sudoku base_board;
 
-/* An instance of these struct-unions */
-struct flags_struct flg;
-union counters_X cnt_X;
-union counters_Y cnt_Y;
+/* An instance of useful struct-unions */
+struct flags_struct flg; /* To have knowledge of the received characters */
+union counters_X cnt_X;  /* Counter for X dimension (send/receive) */
+union counters_Y cnt_Y;  /* Counter for Y dimension (send/receive) */
 
 /************************************************************************/
-/* TIMER1_init                                                          */
+/* timer1_init                                                          */
+/* Function to initialize Timer1, to achieve a refresh rate of 30Hz for */
+/*   the LED solving progress bar                                       */
 /************************************************************************/
- void timer1_init(void){
+void timer1_init(void){
 	const auto LED_refresh_rate = 30;
-	TCCR1B |= (0 << CS10) | (1 << CS11) | (0 << CS12);//set clock pre-scaler to 8
+	
+	/* Set prescaler (CK/8) */
+	TCCR1B |= (0 << CS10) | (1 << CS11) | (0 << CS12);
+	
+	/* Set value of Output Compare Register */
 	OCR1A = (F_CPU / LED_refresh_rate + (F_CPU % LED_refresh_rate > LED_refresh_rate/2))/8;
-	TIMSK |= (1 << OCIE1A); //Timer/Counter1, Output Compare A Match Interrupt Enable
+	
+	/* Timer/Counter1, Output Compare A Match Interrupt Enable */
+	TIMSK |= (1 << OCIE1A);
+	
+	/* Waveform Generation Mode = Clear Timer on compare match OCR1A */ 
 	TCCR1A |= 0;
-	TCCR1B |= (1 << WGM12);//Waveform Generation Mode = Clear Timer on compare match OCR1A
+	TCCR1B |= (1 << WGM12);
  }
  
  /************************************************************************/
- /* Interrupt handler for the LEDs                                       */
+ /* The interrupt service routine for the Timer1 Compare Match event     */
+ /*  Updates the LED bar (based on a LUT in flash)                       */
  /************************************************************************/
 ISR(TIMER1_COMPA_vect){
 	PORTA = pgm_read_byte(&led_bar_LUT[base_board.get_solved_cell_counter()]);
 };
 
-
-
-
 /************************************************************************/
-/* USART_init (C equivalent of usart_init in Lab3)                      */
+/* USART_init                                                           */
+/* Based on AVR manual, pg. 149-150/357                                 */
 /* Function to initialize the serial port                               */
 /************************************************************************/
-void USART_init (unsigned int ubrr){
+void USART_init(unsigned int ubrr){
 	/* Configure baud rate */
 	UBRRH = (unsigned char)(ubrr>>8);
 	UBRRL = (unsigned char)ubrr;
@@ -67,7 +76,6 @@ void USART_init (unsigned int ubrr){
 	 */
 	UCSRB = (1<<RXEN)|(1<<TXEN)|(1<<RXCIE);
 	
-	
 	/* Configure UCSRC (pg 166/357)
 	 URSEL   = 1 (to write UCSRC and not UBRRH)
 	 UMSEL   = 0 (Asynchronous Operation Mode)
@@ -79,10 +87,10 @@ void USART_init (unsigned int ubrr){
 }
 
 /************************************************************************/
-/* ports_init                                                           */
+/* portA_init                                                           */
 /* Function to initialize Port A for the LEDs                           */
 /************************************************************************/
-void portA_init (void){
+void portA_init(void){
 	
 	/* PortA - which segments will light */
 	DDRA = 0xFF;   /* PortA will be output */
@@ -104,6 +112,9 @@ void portA_init (void){
 /* Based on AVR manual, pg. 151/357                                     */
 /*                                                                      */
 /* Polling is needed to ensure that UDR is available to write data      */
+/* In our program, because of the low baud rate, the transmit buffer    */
+/*   should be always empty. So this while loop (in the general case)   */
+/*	 will not execute                                                   */                         
 /************************************************************************/
 void USART_Transmit(unsigned char data){
 	/* Wait for empty transmit buffer */
@@ -112,7 +123,7 @@ void USART_Transmit(unsigned char data){
 	
 	/* Ready to transmit: Put the data into buffer, send the data */
 	#ifdef SIMULATION_MODE
-		TCNT0 = data;
+		TCNT0 = data; /* In simulation mode, read the data from TCNT0 (timer 0 is not used) */
 	#endif
 	#ifndef SIMULATION_MODE
 		UDR = data; 
@@ -121,13 +132,10 @@ void USART_Transmit(unsigned char data){
 }
 
 /************************************************************************/
-/* send_ok (C equivalent of send_ok in Lab3, but here a function        */
-/*             is used to transmit)                                     */
+/* send_ok                                                              */
 /* Function to send an OK<CR><LF> message                               */
 /************************************************************************/
-/* May convert to macro (to reduce function calls) */
-void send_ok (void){
-	
+void send_ok(void){
 	/* Clean the flag registers, because we ended decoding this command */
 	clear_char_flags();
 	
@@ -141,17 +149,17 @@ void send_ok (void){
 
 /************************************************************************/
 /* send_cell                                                            */
+/* Function to send the contents of a cell                              */
+/*    (a N<X><Y><VALUE><CR><LF> command)                                */
 /************************************************************************/
-void send_cell (uint8_t bcd_x, uint8_t bcd_y){
+void send_cell(uint8_t bcd_x, uint8_t bcd_y){
 	/* Clean the flag registers, because we ended decoding this command */
 	clear_char_flags();
 	
 	/* Find the value, and convert it to ASCII */
-	/* Subtract 1 from x and y, because in the assignment X and Y start from 1, but
-	     in C, array positions start from 0. */
 	uint8_t value_to_send =  base_board.get_cell(bcd_y, bcd_x) + 0x30;
 	
-	/* Send the cell data */
+	/* Send the cell's data */
 	USART_Transmit(0x4E);          // N
 	USART_Transmit(bcd_x + 0x30);  // <X>
 	USART_Transmit(bcd_y + 0x30);  // <Y>
@@ -162,12 +170,13 @@ void send_cell (uint8_t bcd_x, uint8_t bcd_y){
 
 /************************************************************************/
 /* send_done                                                            */
+/* Function to send an D<CR><LF> message                               */
 /************************************************************************/
-void send_done (void){
-	/* CLEAN FLAG REGS, OR NOT */
+void send_done(void){
+	/* Clean the flag registers, because we ended decoding this command */
 	clear_char_flags();
 	
-	/* Send a D<CR><LF> message */  /* Clear flag regs is not needed here */
+	/* Send a D<CR><LF> message */
 	USART_Transmit(0x44); // D
 	USART_Transmit(0x0D); // <CR>
 	USART_Transmit(0x0A); // <LF> 
@@ -185,17 +194,15 @@ void clear(void){
 	
 	/* Turn off the LEDs (0 cells completed) */
 	PORTA = 0xFF;
-
 }
 
 /************************************************************************/
 /* The interrupt service routine for the UART receive event             */
-/* (C equivalent of USART_RXC in Lab3)                                  */
 /************************************************************************/
 ISR (USART_RXC_vect){
 	unsigned char received_char = UDR;
 	
-	#ifdef SIMULATION_MODE
+	#ifdef SIMULATION_MODE /* In simulation mode, read the input data from TCNT2 (timer 2 is not used) */
 		received_char = UDR;  /* Read UDR one more time, to consume RXC flag */
 		received_char = TCNT2;
 	#endif
@@ -209,11 +216,11 @@ ISR (USART_RXC_vect){
 			break;
 		case 0x43:
 			flg.received_C = true;
-			clear();
+			clear(); /* On "C" command, clear the grid */
 			break;
 		case 0x4E:
 			flg.received_N = true;
-			args_counter = 0;
+			args_counter = 0; /* On "N" command from PC, three arguments will be received: X, Y, VALUE */
 			break;
 		case 0x50:
 			flg.received_P = true;
@@ -226,7 +233,7 @@ ISR (USART_RXC_vect){
 			break;
 		case 0x44:
 			flg.received_D = true;
-			args_counter = 0;
+			args_counter = 0; /* On "D" command from PC, two arguments will be received: X, Y */
 			break;
 		case 0x4F:
 			flg.received_O = true;
@@ -235,20 +242,19 @@ ISR (USART_RXC_vect){
 			flg.received_K = true;
 			break;
 		case 0x0D:
-			break;
+			break; /* When receiving "<CR>" (for whichever command), nothing has to be done. */
 		case 0x0A:
-			/* If "<LF>", process and return
+			/* When receiving "<LF>", process and return
 			   <LF> is the last character of each command, so it 
 				   is time to execute the received command */
 		
 			if( (flg.received_A && flg.received_T) || (flg.received_C) || (flg.received_N) ){
-				/* If command was AT or C or N, send OK */
+				/* If command was AT or C or N, simply send OK */
 				send_ok();
 			}
 			else if(flg.received_P){
 				/* If command was P,start solving the sudoku after sending OK */
-				//flg.solving_barrier = true;
-				base_board.set_solving_barrier(true);
+				base_board.set_solving_barrier(true); /* When returning to main, the solving will start */
 				send_ok();
 			}
 			else if(flg.received_S){
@@ -261,72 +267,67 @@ ISR (USART_RXC_vect){
 			}
 			else if(flg.received_T){
 				/* If command was T, send next value */
-				if(flg.transmit_barrier){
+				if(flg.transmit_barrier){ /* If transmit barrier is false, this means "break transmission" */
 					cnt_X.sent_counter_X++;
 					if(cnt_X.sent_counter_X == 10){
 						cnt_X.sent_counter_X = 1;
 						cnt_Y.sent_counter_Y++;
 					}
 					send_cell(cnt_X.sent_counter_X, cnt_Y.sent_counter_Y);
+					if((cnt_X.sent_counter_X == 9) && (cnt_Y.sent_counter_Y == 9)){
+						send_done();
+					}
 				}
-				/* Of course, if break_transmission=0(after a B command), we will not send something back to the computer */
-			
 			}
 			else if(flg.received_B){
-				/* If command was B, stop calculations or sending results and send OK */
+				/* If command was B, stop solving or transmitting results and send OK */
 				base_board.set_solving_barrier(false);
 				flg.transmit_barrier = false;
 				send_ok();
 			}
 			else if(flg.received_D){
 				/* If command was D, send cell contents (they are already prepared when we received Y) */
-				send_cell(cnt_X.sent_counter_X, cnt_Y.sent_counter_Y);
+				send_cell(cnt_X.received_X, cnt_Y.received_Y);
 			}
 			else if(flg.received_O && flg.received_K){
 				/* If command was OK, this means that the computer has received all the values */
-				/* TODO */ /*Is something needed here ? */
+				/* Nothing should be done here */
 			}
-			
 			break;
 			
 		default:
 			/* Received numbers (Commands N and D from computer) */
 		
 			if(flg.received_N){ /* If command was N, the first time read X, the second time read Y, the third time read VALUE and store it */
-				if(args_counter == 0){                 /* First argument, X */
+				if(args_counter == 0){                       /* First argument, X */
 					cnt_X.received_X = received_char & 0x0F; /* Convert from ASCII to BCD, using the appropriate mask */
 					args_counter++;
 				}
-				else if(args_counter == 1){            /* Second argument, Y */
+				else if(args_counter == 1){                  /* Second argument, Y */
 					cnt_Y.received_Y = received_char & 0x0F; /* Convert from ASCII to BCD, using the appropriate mask */
 					args_counter++;
 				}
-				else if(args_counter == 2){            /* Third argument, VALUE */
+				else if(args_counter == 2){               /* Third argument, VALUE */
 					received_char = received_char & 0x0F; /* Convert from ASCII to BCD, using the appropriate mask */
 					base_board.set_cell(cnt_Y.received_Y, cnt_X.received_X, received_char);
-					//grid[cnt_Y.received_Y-1][cnt_X.received_X-1] = received_char; /* Subtract 1 because in C, array index starts from 0, but in our UART protocol, it starts from 1 */
-					//num_cnt++; /* Note that we received one more value */
-					//PORTA = pgm_read_byte(&led_bar_LUT[base_board.get_solved_cell_counter()]);
 				}
 			}
-			else if(flg.received_D){ /* If command was D, the first time read X, the second time read Y, the third time prepare VALUE to be sent */
-				 if(args_counter == 0){                 /* First argument, X */
+			else if(flg.received_D){ /* If command was D, the first time read X, the second time read Y */
+				 if(args_counter == 0){                       /* First argument, X */
 					 cnt_X.received_X = received_char & 0x0F; /* Convert from ASCII to BCD, using the appropriate mask */
 					 args_counter++;
 				 }
-				 else if(args_counter == 1){            /* Second argument, Y */
+				 else if(args_counter == 1){                  /* Second argument, Y */
 					 cnt_Y.received_Y = received_char & 0x0F; /* Convert from ASCII to BCD, using the appropriate mask */	 
 				 }
-			}
-		
-		
-		
+			}	
 	}
 		
 }
 
 /************************************************************************/
-/*                                                                      */
+/* clear_char_flags                                                     */
+/* Function to set all the character receiving flags to false           */
 /************************************************************************/
 void clear_char_flags(void){
 	flg.received_A = false;
@@ -343,8 +344,7 @@ void clear_char_flags(void){
 
 /************************************************************************/
 /* init                                                                 */
-/* Function to initialize global variables, timers,ports, counters      */
-/*     and USART                                                        */
+/* Function to initialize global variables, timer1, portA and USART     */
 /************************************************************************/
 void init(void){
 	
@@ -352,19 +352,18 @@ void init(void){
 	args_counter = 0;
 	cnt_X.received_X = 0x00;
 	cnt_Y.received_Y = 0x00;
-	
 	cnt_X.sent_counter_X = 1;
 	cnt_Y.sent_counter_Y = 1;
 	
+	/* Clear the flags */
 	clear_char_flags();
-	base_board.set_solving_barrier(false); //flg.solving_barrier = false;
+	base_board.set_solving_barrier(false);
 	flg.transmit_barrier = false;
-	
 	
 	/* Configure and initialize PortA */
 	portA_init();
 	
-	/* Initialize Timer1 (sed to refresh the LED progress bar) */
+	/* Initialize Timer1 (used to refresh the LED progress bar) */
 	timer1_init();
 	
 	/* Initialize memory and PortA */
